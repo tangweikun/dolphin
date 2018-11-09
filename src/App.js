d3724911e485ed386fdb8b6f62bc0424eaf2ad27
tangweikun <819573105@qq.com>
2018-11-09 15:15:58 +0800

    :wrench: config staff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { MDXProvider } from '@mdx-js/tag'
+import { Container, Provider as RebassProvider } from 'rebass'
+import createComponents from '@rebass/markdown'
+
+import Hello from './hello.md'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <MDXProvider components={createComponents()}>
+        <RebassProvider>
+          <Container>
+            <Hello />
+          </Container>
+        </RebassProvider>
+      </MDXProvider>
+    )
   }
 }
 
-export default App;
+export default App
